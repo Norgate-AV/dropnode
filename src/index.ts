e3e4ca2966210538adfd98aaf69d613dad308eb6
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2023-10-06 00:32:27 +0100

    feat: add initial functionality

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,88 @@
-import "dotenv/config";
+#!/usr/bin/env node
 
-console.log(`Running in ${process.env.NODE_ENV} mode`);
-console.log("Hello, World!");
+import "dotenv/config";
+import execa from "execa";
+import { resolve } from "path";
+import { glob } from "glob";
+import chalk from "chalk";
+import { checkbox } from "@inquirer/prompts";
+import { ListrInquirerPromptAdapter } from "@listr2/prompt-adapter-inquirer";
+import { Listr } from "listr2";
+
+interface Context {
+    paths: Array<string>;
+}
+
+(async () => {
+    try {
+        const cwd = process.cwd();
+
+        const tasks = new Listr<Context>(
+            [
+                {
+                    title: "Searching for node_modules",
+                    task: async (ctx) => {
+                        const paths = await glob(`${cwd}/**/node_modules`);
+
+                        if (paths.length === 0) {
+                            console.log(chalk.yellow("No node_modules found"));
+                            process.exit(0);
+                        }
+
+                        ctx.paths = paths.filter(
+                            (path) =>
+                                path.indexOf("node_modules") ===
+                                path.lastIndexOf("node_modules"),
+                        );
+                    },
+                },
+                {
+                    task: async (ctx, task) => {
+                        ctx.paths = await task
+                            .prompt(ListrInquirerPromptAdapter)
+                            .run(checkbox, {
+                                message: "Select the node_modules to ignore",
+                                choices: ctx.paths.map((path) =>
+                                    path.replace(cwd, ""),
+                                ),
+                                pageSize: 20,
+                            });
+                    },
+                },
+                {
+                    title: "Ignoring node_modules",
+                    task: async (ctx) => {
+                        for (let path of ctx.paths) {
+                            path = resolve(cwd, path);
+
+                            const getCommand = () => {
+                                switch (process.platform) {
+                                    case "win32":
+                                        return {
+                                            program: "powershell.exe",
+                                            args: [
+                                                "-Command",
+                                                `Set-Content -Path '${path}' -Stream com.dropbox.ignored -Value 1`,
+                                            ],
+                                        };
+                                    default:
+                                        throw new Error("Unknown platform");
+                                }
+                            };
+
+                            const command = getCommand();
+                            await execa(command.program, command.args);
+                        }
+                    },
+                },
+            ],
+            { concurrent: false },
+        );
+
+        await tasks.run();
+        console.log(chalk.green("Complete!"));
+    } catch (error) {
+        console.error(chalk.red(error));
+        process.exit(1);
+    }
+})();
