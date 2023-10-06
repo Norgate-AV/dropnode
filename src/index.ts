3860b93283ab91c070934ffd4214ab4d3b814ab4
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2023-10-06 02:37:04 +0100

    feat: add Listr prompt plugin for Inquirer

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,24 @@
 
 import "dotenv/config";
 import execa from "execa";
+import os from "os";
 import { resolve } from "path";
 import { glob } from "glob";
 import chalk from "chalk";
 import { checkbox } from "@inquirer/prompts";
 import { ListrInquirerPromptAdapter } from "@listr2/prompt-adapter-inquirer";
 import { Listr } from "listr2";
+import isWsl from "is-wsl";
 
-interface Context {
-    paths: Array<string>;
+interface Ctx {
+    paths: string[];
 }
 
 (async () => {
     try {
         const cwd = process.cwd();
 
-        const tasks = new Listr<Context>(
+        const tasks = new Listr<Ctx>(
             [
                 {
                     title: "Searching for node_modules",
@@ -37,16 +39,22 @@ interface Context {
                     },
                 },
                 {
-                    task: async (ctx, task) => {
-                        ctx.paths = await task
-                            .prompt(ListrInquirerPromptAdapter)
+                    title: "Select node_modules to ignore",
+                    task: async (ctx, task): Promise<string[]> => {
+                        const paths = await task
+                            .prompt<ListrInquirerPromptAdapter>(
+                                ListrInquirerPromptAdapter,
+                            )
                             .run(checkbox, {
-                                message: "Select the node_modules to ignore",
-                                choices: ctx.paths.map((path) =>
-                                    path.replace(cwd, ""),
-                                ),
+                                message: "Select node_modules to ignore",
+                                choices: ctx.paths.map((path) => ({
+                                    name: path.replace(cwd, ""),
+                                    value: path,
+                                })),
                                 pageSize: 20,
                             });
+
+                        return paths as string[];
                     },
                 },
                 {
@@ -64,14 +72,65 @@ interface Context {
                                                 "-Command",
                                                 `Set-Content -Path '${path}' -Stream com.dropbox.ignored -Value 1`,
                                             ],
+                                            options: {},
                                         };
+                                    case "darwin":
+                                        return {
+                                            program: "xattr",
+                                            args: [
+                                                "-w",
+                                                "com.dropbox.ignored",
+                                                "1",
+                                                `"${path.replace(
+                                                    `${cwd}/`,
+                                                    "",
+                                                )}"`,
+                                            ],
+                                            options: {
+                                                uid: os.userInfo().uid,
+                                            },
+                                        };
+                                    case "linux":
+                                        return isWsl
+                                            ? {
+                                                  program: "powershell.exe",
+                                                  args: [
+                                                      "-Command",
+                                                      `Set-Content -Path '${path.replace(
+                                                          `/mnt/${
+                                                              path.split("/")[2]
+                                                          }`,
+                                                          `${
+                                                              path.split("/")[2]
+                                                          }:`,
+                                                      )}' -Stream com.dropbox.ignored -Value 1`,
+                                                  ],
+                                                  options: {},
+                                              }
+                                            : {
+                                                  program: "attr",
+                                                  args: [
+                                                      "-s",
+                                                      "com.dropbox.ignored",
+                                                      "-V",
+                                                      "1",
+                                                      path,
+                                                  ],
+                                                  options: {},
+                                              };
                                     default:
-                                        throw new Error("Unknown platform");
+                                        throw new Error(
+                                            "Unknown Operating System",
+                                        );
                                 }
                             };
 
                             const command = getCommand();
-                            await execa(command.program, command.args);
+                            await execa(
+                                command.program,
+                                command.args,
+                                command.options,
+                            );
                         }
                     },
                 },
